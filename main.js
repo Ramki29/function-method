48b32286245db009985e02a307a80a5367d02006
Ramki29 <ramkie7010@gmail.com>
2023-03-24 21:31:53 +0530

    add function method

---
+++ main.js
@@ -0,0 +1,40 @@
+//push 
+var number=[1,2,3,4];
+number.push(5,6);
+console.log(number);
+console.log("<br>");
+
+//pop
+let names=["key", "boy", "hat", "toy"]
+names.pop();
+console.log(names);
+console.log("<br>");
+
+//shift
+var numbers=[1,2,3,4,5];
+numbers.shift();
+console.log(numbers);
+console.log("<br>");
+
+//unshift
+var num1=[20,30,40];
+num1.unshift(10);
+console.log(num1);
+
+//slice
+var a=[1,2,3,4,5,6,7,8,9];
+var b=a.slice(3,6);
+console.log(b);
+
+//lastIndexOf
+var c=[1,2,3,4,5,7,6,8,9,6,10,5,4,6];
+var d=c.lastIndexOf(5);
+console.log(d);
+
+//concat
+var e=[10,20,30];
+var f=[40,50,60];
+var g=e.concat(f);
+console.log(g);
+
+
